d54f5bae12b1907bb63c32c8623d9be27949516d
Lordian Ayles <gkai70263@gmail.com>
2026-07-06 17:34:43 -0400

    format+new: cooldowns feature

--- lua/autorun/server/tripping.lua
+++ lua/autorun/server/tripping.lua
@@ -5,6 +5,8 @@ local TripTimeThreshold = CreateConVar("npc_trip_time", "5", { FCVAR_ARCHIVE, FC
     "Time in seconds before a tripped NPC gets up")
 local TripRaycastLength = CreateConVar("npc_trip_ray_length", "50", { FCVAR_ARCHIVE, FCVAR_REPLICATED, FCVAR_NOTIFY },
     "Distance to check for props at NPC feet")
+local TripCooldown = CreateConVar("npc_trip_cooldown", "3", { FCVAR_ARCHIVE, FCVAR_REPLICATED, FCVAR_NOTIFY },
+    "Seconds before an NPC can trip again")
 
 local function CreateRagdollFromNPC(npc)
     if not IsValid(npc) then return end
@@ -37,112 +39,117 @@ local function CreateRagdollFromNPC(npc)
     end
 
     rag:SetCollisionGroup(COLLISION_GROUP_PASSABLE_DOOR)
+
+    if IsValid(npc:GetActiveWeapon()) then
+        npc:GetActiveWeapon():Remove()
+    end
     npc:Remove()
 
     return rag
 end
 
 local trippedNPCs = {}
+local nextCheck = 0
 
 hook.Add("Think", "NPCTripping_Check", function()
     if not TripEnabled:GetBool() then return end
 
-    for _, ent in ents.Iterator() do
-        if not ent:IsNPC() or trippedNPCs[ent] then continue end
-        if not IsValid(ent) or not ent:IsNPC() or ent:Health() <= 0 then continue end
+    local currentTime = CurTime()
 
-        local pos = ent:GetPos()
-        local forward = ent:GetForward()
-        local down = Vector(0, 0, -1)
+    if currentTime >= nextCheck then
+        nextCheck = currentTime + 0.1
 
-        local startPos = pos + Vector(0, 0, 10) -- Slightly above feet
-        local endPos = startPos + forward * TripRaycastLength:GetFloat() + down * 5
+        for _, ent in ents.Iterator() do
+            if not IsValid(ent) or not ent:IsNPC() or ent:Health() <= 0 then continue end
+            if ent.NPC_TripCooldown and currentTime < ent.NPC_TripCooldown then continue end
 
-        local trace = util.TraceLine({
-            start = startPos,
-            endpos = endPos,
-            filter = { ent, ent:GetActiveWeapon() }, -- Ignore self and weapon
-            mask = MASK_SHOT
-        })
+            local pos = ent:GetPos()
+            local forward = ent:GetForward()
+            local startPos = pos + Vector(0, 0, 10)
+            local endPos = startPos + forward * TripRaycastLength:GetFloat() + Vector(0, 0, -5)
 
-        if trace.Hit
-            and trace.Entity
-            and trace.Entity:IsValid()
-            and (trace.Entity:GetClass() == "prop_physics"
-                or trace.Entity:GetClass() == "prop_ragdoll") then
-            local weapon        = ent:GetActiveWeapon()
-            local weaponClass   = weapon and weapon:IsValid() and weapon:GetClass() or nil
-            local npcClass      = ent:GetClass()
-            local oldModel      = ent:GetModel()
+            local trace = util.TraceLine({
+                start = startPos,
+                endpos = endPos,
+                filter = { ent, ent:GetActiveWeapon() },
+                mask = MASK_SHOT
+            })
 
-            local npcVelocity   = ent:GetVelocity()
-            local forwardForce  = forward * (math.max(npcVelocity:Length(), 150) * 1.5)
-            local upwardForce   = Vector(0, 0, 80)
-            local totalVelocity = npcVelocity + forwardForce + upwardForce
+            if trace.Hit and IsValid(trace.Entity) and
+                (trace.Entity:GetClass() == "prop_physics" or trace.Entity:GetClass() == "prop_ragdoll") then
+                local weapon = ent:GetActiveWeapon()
+                local weaponClass = IsValid(weapon) and weapon:GetClass() or nil
+                local npcClass = ent:GetClass()
+                local oldModel = ent:GetModel()
 
-            local ragdoll       = CreateRagdollFromNPC(ent)
-            if ! ragdoll or not IsValid(ragdoll) or ! ent:Alive() then return end
+                local npcVelocity = ent:GetVelocity()
+                local forwardForce = forward * (math.max(npcVelocity:Length(), 150) * 1.5)
+                local upwardForce = Vector(0, 0, 80)
+                local totalVelocity = npcVelocity + forwardForce + upwardForce
 
-            for i = 0, ragdoll:GetPhysicsObjectCount() - 1 do
-                local phys = ragdoll:GetPhysicsObjectNum(i)
-                if IsValid(phys) then
-                    phys:SetVelocity(totalVelocity)
+                local health = ent:Health()
+                local maxHealth = ent:GetMaxHealth()
 
-                    if i == 0 then -- Head/Chest area usually
-                        phys:AddAngleVelocity(ent:GetRight() * 15)
+                local ragdoll = CreateRagdollFromNPC(ent)
+                if not IsValid(ragdoll) then continue end
+
+                for i = 0, ragdoll:GetPhysicsObjectCount() - 1 do
+                    local phys = ragdoll:GetPhysicsObjectNum(i)
+                    if IsValid(phys) then
+                        phys:SetVelocity(totalVelocity)
+                        if i == 0 then
+                            phys:AddAngleVelocity(ent:GetRight() * 15)
+                        end
                     end
                 end
+
+                trippedNPCs[ragdoll] = {
+                    trippedTime = currentTime,
+                    oldClass = npcClass,
+                    oldWeapon = weaponClass,
+                    oldModel = oldModel,
+                    TripHealth = health,
+                    TripMaxHealth = maxHealth
+                }
             end
-
-            trippedNPCs[ragdoll] = {
-                trippedTime = CurTime(),
-                oldClass = npcClass,
-                oldWeapon = weaponClass,
-                oldModel = oldModel,
-                TripHealth = ent.Health and ent:Health() or 100
-            }
-
-            ent:Remove()
         end
     end
 
     for ragdoll, data in pairs(trippedNPCs) do
-        if not ragdoll or not ragdoll:IsValid() then
+        if not IsValid(ragdoll) then
             trippedNPCs[ragdoll] = nil
             continue
         end
 
-        if CurTime() - data.trippedTime >= TripTimeThreshold:GetFloat() then
+        if currentTime - data.trippedTime >= TripTimeThreshold:GetFloat() then
             local newNPC = ents.Create(data.oldClass)
 
-            if not newNPC or not newNPC:IsValid() then
+            if not IsValid(newNPC) then
                 trippedNPCs[ragdoll] = nil
                 ragdoll:Remove()
                 continue
             end
 
-            PrintTable(data)
-
             newNPC:SetPos(ragdoll:GetPos() + Vector(0, 0, 10))
             newNPC:SetAngles(ragdoll:GetAngles())
-            newNPC:SetHealth(data.TripHealth)
             newNPC:Spawn()
 
             newNPC:SetModel(data.oldModel)
             newNPC:SetSkin(ragdoll:GetSkin())
+            newNPC:SetMaxHealth(data.TripMaxHealth)
+            newNPC:SetHealth(data.TripHealth)
+            newNPC.NPC_TripCooldown = currentTime + TripCooldown:GetFloat()
 
-            -- 3. Restore those bodygroups we talked about earlier
             for i = 0, ragdoll:GetNumBodyGroups() - 1 do
                 newNPC:SetBodygroup(i, ragdoll:GetBodygroup(i))
             end
 
             if data.oldWeapon then
                 timer.Simple(0.1, function()
-                    if newNPC and newNPC:IsValid() then
+                    if IsValid(newNPC) then
                         newNPC:Give(data.oldWeapon)
-
                         timer.Simple(0.05, function()
-                            if newNPC and newNPC:IsValid() then
+                            if IsValid(newNPC) then
                                 newNPC:SelectWeapon(data.oldWeapon)
                             end
                         end)
@@ -160,7 +167,7 @@ concommand.Add("npc_trip_list", function()
     if not TripEnabled:GetBool() then return end
     print("=== Tripped NPCs ===")
     for ragdoll, data in pairs(trippedNPCs) do
-        if ragdoll and ragdoll:IsValid() then
+        if IsValid(ragdoll) then
             local timeLeft = (data.trippedTime + TripTimeThreshold:GetFloat() - CurTime())
             print(string.format("Ragdoll %s - Class: %s, Gets up in %.1fs",
                 ragdoll:EntIndex(), data.oldClass, timeLeft))
@@ -173,8 +180,8 @@ end)
 
 hook.Add("EntityTakeDamage", "CheckAndKill", function(target, dmg)
     if not TripEnabled:GetBool() then return end
+    if not IsValid(target) or not target:IsRagdoll() then return end
 
-    if ! target:IsRagdoll() then return end
     if not (dmg:IsDamageType(DMG_BULLET) or
             dmg:IsDamageType(DMG_SLASH) or
             dmg:IsDamageType(DMG_BUCKSHOT) or
@@ -182,11 +189,10 @@ hook.Add("EntityTakeDamage", "CheckAndKill", function(target, dmg)
         return
     end
 
-    --- health mod
-    if trippedNPCs[target] then
-        local targRef = trippedNPCs[target]
-        targRef.TripHealth = targRef.TripHealth - dmg:GetDamage()
-        if targRef.TripHealth <= 0 then
+    local data = trippedNPCs[target]
+    if data then
+        data.TripHealth = data.TripHealth - dmg:GetDamage()
+        if data.TripHealth <= 0 then
             trippedNPCs[target] = nil
         end
     end
